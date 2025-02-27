f966e3f3d302327ada5fea76c6c510e4015aea37
jaranatur11 <jara.natur@t-online.de>
2025-02-27 16:53:11 +0100

    prevent earth from floating and increase visibility

--- script.js
+++ script.js
@@ -5,18 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Sicherstellen, dass die Erde fixiert bleibt
     function fixEarthPosition() {
-        earth.setAttribute("position", "0 -0.05 0");
-        earth.setAttribute("constraint", "lock");
+        earth.setAttribute("position", "0 0 0");
+        earth.setAttribute("anchor", "true");
     }
 
     // Falls AR.js das Modell doch bewegt, nach 500ms erneut fixieren
     setTimeout(fixEarthPosition, 500);
-
-    earth.addEventListener("click", () => {
-        console.log("🌍 Erde wurde angeklickt!");
-    });
-
-    earth.addEventListener("touchstart", () => {
-        console.log("🌍 Erde wurde angetippt!");
-    });
 });
