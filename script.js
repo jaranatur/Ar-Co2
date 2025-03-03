df70499758ab254828946aed42120124f12e12e9
jaranatur11 <jara.natur@t-online.de>
2025-03-03 17:30:08 +0100

    hilfe oder so

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     function fixEarthPosition() {
         earth.setAttribute("position", "0 0 0");
         earth.setAttribute("rotation", "0 0 0");
-        earth.setAttribute("scale", "2 2 2"); // Bigger Earth
+        
     }
 
     // Force Earth to reset every 500ms in case AR.js moves it
