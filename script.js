343bc80ce42e8c8e05a5a8c48953cceba436663f
jaranatur11 <jara.natur@t-online.de>
2025-03-03 17:47:27 +0100

    feat: Added Earth rotation animation

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // 🛠 Adjust Earth Size for Mobile
     function adjustScale() {
-        earth.setAttribute("scale", "3 3 3"); 
+        earth.setAttribute("scale", "2 2 2"); 
     }
     
 
